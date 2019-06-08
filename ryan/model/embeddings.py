c6b2d92462032af1b0c3504a86a3093675f2c6f1
Ryan S. Shin <sungjin7127@gmail.com>
2019-06-08 17:50:08 +0900

    update summary

---
+++ ryan/model/embeddings.py
@@ -0,0 +1,20 @@
+"""
+Embedding
+
+"""
+import time
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def SentenceEmbedding(tf.keras.Model):
+
+	def __init__(self, config):
+
+		super(SentenceEmbedding, self).__init__()
+		self.config = config
+
+
+
+
+		print("will be implement")
---
+++ ryan/model/net.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from model.embeddings import SentenceEmbedding
+
+class FCClassifier(tf.keras.Model):
+	"""
+	Natural Language Inference with Hierarchical BiLSTM Max Pooling Architecture (HBMP)
+	"""
+	def __init(self, config):
+		super(FCClassifier, self).__init__()
+		self.config = config
+
+		self.seq_in_size = 4 * config.hidden_dim
+		self.fc_dim = config.fc_dim
+		self.out_dim = config.out_dim
+		self.activation = config.activation # 'relu'
+
+		if self.config.encoder_type == 'BiLSTMMaxPoolEncoder':
+			self.seq_in_size *= 2
+		elif self.config.encoder_type == 'HBMP':
+			self.seq_in_size *= 6
+
+	def mlp(self, x):
+
+		x = layers.Dropout(self.dropout)(x)
+		x = layers.Dense(self.fc_dim, activation=self.activation)(x)
+		x = layers.Dropout(self.dropout)(x)
+		x = layers.Dense(self.fc_dim, activation=self.activation)(x)
+		x = layers.Dense(self.out_dim)(x)
+
+		return x
+
+	def call(self, prem, hypo):
+		features = tf.concat([prem, hypo, tf.abs(prem-hypo), prem*hypo], 1)
+
+		output = self.mlp(features)
+		return output
+
+class NLIModel(tf.keras.Model):
+	"""
+	NLI 테스크의 Main 모델
+	"""
+	def __init__(self, config):
+		super(NLIModel, self).__init__()
+		self.config = config
+		self.sentence_embedding = SentenceEmbedding(config)
+		self.classifier = FCClassifier(config)
+
+	def call(self, x):
+		prem = self.sentence_embedding(x.premise)
+		hypo = self.sentence_embedding(x.hypothesis)
+		answer = self.classifier(prem, hypo)
+
+		return answer
+
+
+
+
+
+
+
+
+
+
+
